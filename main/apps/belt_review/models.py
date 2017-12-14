d203e5e02dcabb215e0ce3b46a90bb16f3c9e3dc
Paul <paulwinegard@gmail.com>
2017-12-14 13:39:20 -0800

    Models created, migrations made.

--- main/apps/belt_review/models.py
+++ main/apps/belt_review/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    review = models.TextField()
+    rating = models.IntegerField()
+    book_reviewers = models.ManyToManyField(User)
+
+
 # Create your models here.
